b8219308118776a92f71ddb9a1ba4643183d3de9
Lorenza Quinzi Carmo <Quinzi@Lorenzas.local>
2017-06-28 02:01:14 -0300

    feat: intro background color

--- myScript.js
+++ myScript.js
@@ -1,15 +1,15 @@
-// Pega os elementos body e a classe do hover
-var elm = document.querySelector('.myname');
-var body = document.querySelector('body')
-
-// Quando o mouse entrar ou sair, chama a função troca classe
-elm.addEventListener('mouseover', trocaClasse2)
-elm.addEventListener('mouseout', trocaClasse2)
-
-// Troca a classe
-function trocaClasse2 (evt) {
-  evt.stopPropagation(); // <-- evita que afete outros elementos
-
-  // troca a classe mesmo
-  body.classList.toggle('myname-active')
-}
+// // Pega os elementos body e a classe do hover
+// var elm = document.querySelector('.myname');
+// var body = document.querySelector('body')
+//
+// // Quando o mouse entrar ou sair, chama a função troca classe
+// elm.addEventListener('mouseover', trocaClasse2)
+// elm.addEventListener('mouseout', trocaClasse2)
+//
+// // Troca a classe
+// function trocaClasse2 (evt) {
+//   evt.stopPropagation(); // <-- evita que afete outros elementos
+//
+//   // troca a classe mesmo
+//   body.classList.toggle('myname-active')
+// }
